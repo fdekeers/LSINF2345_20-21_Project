106cf5a058a5b27e0e81f772ed24c52f5132c5cc
GrosPoulet42 <francois.dekeersmaeker@student.uclouvain.be>
2020-12-01 12:37:51 +0100

    Permute function

--- src/node.erl
+++ src/node.erl
@@ -1,5 +1,6 @@
 - module(node).
-- export([join/1, getNeigs/2, listen/0]).
+- import(utils, [selectPeer/2, propagateView/3]).
+- export([join/1, getNeigs/2, loop/4, listen/0]).
 
 join(BootServerPid) ->
   BootServerPid ! { join, self() },
@@ -14,7 +15,24 @@ getNeigs(BootServerPid, NodeId) ->
     { getPeersOk, Neigs } -> Neigs
   end.
 
+
+%%% PEER SAMPLING %%%
+
+% Active thread
+loop({NodeId, NodePid}, View, Cycle, {Selection, Propagation, H, S}) ->
+  timer:sleep(3000),
+  {PeerPid, _} = utils:selectPeer(View, Selection),
+  utils:propagateView(FromPid, PeerPid, Cycle, View, {Propagation, H}),
+  loop({NodeId, NodePid}, NewView, Cycle + 1, {Selection, Propagation, H, S}).
+
+
+% Passive thread
 listen() ->
   receive
+    {push, View, FromPid} ->
+
+    {pushpull, View, FromPid} ->
+
+
     kill -> ok
   end.
