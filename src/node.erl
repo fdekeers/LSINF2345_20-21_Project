d1a6c33960f4248fe9c4391adc6813a8af064b10
GrosPoulet42 <francois.dekeersmaeker@student.uclouvain.be>
2020-12-03 12:22:26 +0100

    Updated quite a lot of things

--- src/node.erl
+++ src/node.erl
@@ -18,24 +18,28 @@ getNeigs(BootServerPid, NodeId) ->
 
 %%% PEER SAMPLING SERVICE %%%
 
-listen({NodeId, NodePid}, View, {Selection, Propagation, H, S}) ->
+listen(NodeId, View, {Selection, Propagation, H, S}) ->
   receive
+    {init, InitView} ->
+      % Initialization of the node's view.
+      listen(NodeId, InitView, {Selection, Propagation, H, S});
+
     {active, Cycle} ->
-      % First active section, selects the peer to contact, the subset of the
+      % Active section, selects the peer to contact, the subset of the
       % view to send, and send it.
       {PeerPid, _} = utils:selectPeer(View, Selection),
-      Buffer = utils:selectBuffer(NodePid, Cycle, View, H),
-      PeerPid ! {push, FromPid, Cycle, Buffer},
-      listen({NodeId, NodePid}, View, {Selection, Propagation, H, S});
+      Buffer = utils:selectBuffer(self(), Cycle, View, H),
+      PeerPid ! {push, self(), Cycle, Buffer},
+      listen(NodeId, View, {Selection, Propagation, H, S});
 
     {push, FromPid, Cycle, ReceivedBuffer} ->
       % Passive section, updates local view with received view.
       % If strategy is pushpull, first respond to sender with local buffer.
-      NewView = utils:receivedBuffer(NodePid, FromPid, Cycle, View, ReceivedBuffer, {Propagation, H, S}),
-      listen({NodeId, NodePid}, NewView, {Selection, Propagation, H, S});
+      NewView = utils:receivedBuffer(self(), FromPid, Cycle, View, ReceivedBuffer, {Propagation, H, S}),
+      listen(NodeId, NewView, {Selection, Propagation, H, S});
 
     {response, ReceivedBuffer} ->
       % Received response to pushpull message, updates local view.
-      NewView = utils:selectView(NodePid, View, ReceivedBuffer, H, S),
-      listen({NodeId, NodePid}, NewView, {Selection, Propagation, H, S})
+      NewView = utils:selectView(self(), View, ReceivedBuffer, H, S),
+      listen(NodeId, NewView, {Selection, Propagation, H, S})
     end.
--- src/project.erl
+++ src/project.erl
@@ -1,22 +1,29 @@
 - module(project).
 - import(bootstrap_server, [listen/2]).
-- import(node, [join/2, getNeigs/3, listen/0]).
-- export([launch/1]).
+- import(node, [join/2, getNeigs/3, listen/3]).
+- export([launch/3]).
 
-makeNet(N, BootServerPid) -> makeNet(N, BootServerPid, [], 0).
-
-makeNet(N, BootServerPid, Net, Counter) ->
-  NodePid = spawn(node, listen, []),
+% Creates the initial network, and return a list of all the nodes with their Pid.
+% More "Erlang-friendly" adaptation of the initial makeNet function.
+makeNet(N, BootServerPid, Params) ->
+  makeNet(N, BootServerPid, Params, []).
+makeNet(0, _, _, Nodes) ->
+  lists:reverse(Nodes);
+makeNet(N, BootServerPid, Params, Nodes) ->
   NodeId = node:join(BootServerPid),
-  Node = { NodeId, NodePid },
-  if
-    N =/= Counter + 1 ->
-      makeNet(N, BootServerPid, Net ++ [ Node ], Counter + 1);
-    N =:= Counter + 1 ->
-      Net ++ [ Node ]
-  end.
+  NodePid = spawn(node, listen, [NodeId, [], Params]),
+  Node = {NodeId, NodePid},
+  makeNet(N-1, BootServerPid, Params, [Node|Nodes]).
 
-launch(N) ->
+
+%%% START THE PROJECT %%%
+
+launch(tree, N, Params) ->
   % Creates server with an empty tree
-  BootServerPid = spawn(bootstrap_server, listen, [ 0, {} ]),
-  makeNet(N, BootServerPid).
+  BootServerPid = spawn(bootstrap_server, listenT, [ 0, {} ]),
+  Nodes = makeNet(N, BootServerPid, Params),
+  FunInitNodes = fun({NodeId, NodePid}) ->
+    {Neighbors} = node:getNeigs(BootServerPid, NodeId),
+    io:format("Neighbors of ~p: ~p.~n", [NodeId, Neighbors])
+  end,
+  lists:foreach(FunInitNodes, Nodes).
