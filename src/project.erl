c290b173cea5586b2d4398087875008de528e04c
GrosPoulet42 <francois.dekeersmaeker@student.uclouvain.be>
2020-12-03 22:36:01 +0100

    Round the number of nodes to ceiling

--- src/project.erl
+++ src/project.erl
@@ -3,7 +3,6 @@
 - import(node, [join/2, getNeigs/3, listen/4]).
 - export([launch/3]).
 
-
 % Creates the initial network, and return a list of all the nodes with their Pid.
 % More "Erlang-friendly" adaptation of the initial makeNet function.
 makeNet(N, BootServerPid, Params) ->
@@ -49,36 +48,49 @@ scenario(Nodes) ->
 
 scenario(AllNodes, [], AllNodes, 0) ->
   % First cycle, bootstrapping phase
-  N = round(0.4 * length(AllNodes)),
+  N = round(math:ceil(0.4 * length(AllNodes))),
   {ActiveNodes, InactiveNodes} = startNodes(N, 0, [], AllNodes),
+  io:format("Cycle ~p, ~p active nodes~n", [0, length(ActiveNodes)]),
+  activateNodes(ActiveNodes, 0),
   scenario(AllNodes, ActiveNodes, InactiveNodes, 1);
 
 scenario(AllNodes, ActiveNodes, InactiveNodes, 120) ->
   % 120th cycle
   % As 120 is divisible by 30, start 20% of the inactive nodes
-  N1 = round(0.2 * length(InactiveNodes)),
+  N1 = round(math:ceil(0.2 * length(InactiveNodes))),
   {NewActiveNodes, NewInactiveNodes} = startNodes(N1, 120, ActiveNodes, InactiveNodes),
 
   % Crash a random number of active nodes, between 50 and 70% of the active nodes
   Ratio = (rand:uniform(21) + 49) / 100,
-  N2 = round(Ratio * length(NewActiveNodes)),
+  N2 = round(math:ceil(Ratio * length(NewActiveNodes))),
   {NewActiveNodes2, NewInactiveNodes2} = crashNodes(N2, NewActiveNodes, NewInactiveNodes),
+
+  % Activate active nodes
+  io:format("Cycle ~p, ~p active nodes~n", [120, length(NewActiveNodes2)]),
+  activateNodes(NewActiveNodes2, 120),
   scenario(AllNodes, NewActiveNodes2, NewInactiveNodes2, 121);
 
-scenario(_, _, _, 180) ->
+scenario(_, ActiveNodes, _, 180) ->
   % End of the scenario
+  io:format("Cycle ~p, ~p active nodes~n", [180, length(ActiveNodes)]),
   stop;
 
 scenario(AllNodes, ActiveNodes, InactiveNodes, Cycle) ->
+  % General case
   DivisibleBy30 = Cycle rem 30 =:= 0,
   if
     DivisibleBy30 ->
       % Growing phase, start 20% of the inactive nodes
-      N = round(0.2 * length(InactiveNodes)),
+      N = round(math:ceil(0.2 * length(InactiveNodes))),
       {NewActiveNodes, NewInactiveNodes} = startNodes(N, Cycle, ActiveNodes, InactiveNodes),
+      % Activate active nodes
+      io:format("Cycle ~p, ~p active nodes~n", [Cycle, length(NewActiveNodes)]),
+      activateNodes(NewActiveNodes, Cycle),
       scenario(AllNodes, NewActiveNodes, NewInactiveNodes, Cycle+1);
     true ->
-      % Nothing special to do
+      % Nothing special to do, only activate active nodes
+      io:format("Cycle ~p, ~p active nodes~n", [Cycle, length(ActiveNodes)]),
+      activateNodes(ActiveNodes, Cycle),
       scenario(AllNodes, ActiveNodes, InactiveNodes, Cycle+1)
   end.
   %timer:sleep(3000),
@@ -91,9 +103,14 @@ startNodes(N, Cycle, ActiveNodes, InactiveNodes) ->
   {NodeId, NodePid} = utils:pickRandom(InactiveNodes),
   NewInactiveNodes = lists:delete({NodeId, NodePid}, InactiveNodes),
   NodePid ! {start},
-  NodePid ! {active, Cycle},
   NewActiveNodes = [{NodeId, NodePid}|ActiveNodes],
   startNodes(N-1, Cycle, NewActiveNodes, NewInactiveNodes).
+
+% Activate all the active nodes, i.e. start the active thread of each node
+% This function is called at every cycle.
+activateNodes(ActiveNodes, Cycle) ->
+  FunActivate = fun({_, Pid}) -> Pid ! {active, Cycle} end,
+  lists:foreach(FunActivate, ActiveNodes).
 
 % Crashes N nodes, and updates the active and inactive nodes lists.
 crashNodes(0, ActiveNodes, InactiveNodes) ->
