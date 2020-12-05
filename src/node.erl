b4706bdc6ad7f1269d64f5292911df8d254de1e8
GrosPoulet42 <francois.dekeersmaeker@student.uclouvain.be>
2020-12-05 09:52:46 +0100

    Log function

--- src/node.erl
+++ src/node.erl
@@ -1,6 +1,9 @@
 - module(node).
 - import(utils, [selectPeer/2, propagateView/3]).
-- export([join/1, getNeigs/2, listen/4]).
+- export([join/1,
+          getNeigs/2,
+          listen/4,
+          log/3]).
 
 join(BootServerPid) ->
   BootServerPid ! { join, self() },
@@ -39,20 +42,43 @@ listen(NodeId, up, View, {Selection, Propagation, H, S}) ->
       {_, PeerPid, _} = utils:selectPeer(View, Selection),
       {PermutedView, Buffer} = utils:selectBuffer(NodeId, self(), Cycle, View, H),
       PeerPid ! {push, self(), Cycle, Buffer},
-      listen(NodeId, up, PermutedView, {Selection, Propagation, H, S});
+      NewView = listen(NodeId, waiting, PermutedView, {Selection, Propagation, H, S}),
+      log(Cycle, NodeId, NewView),
+      listen(NodeId, up, NewView, {Selection, Propagation, H, S});
+    {kill} ->
+      % Stop the peer-sampling service on the node
+      listen(NodeId, down, View, {Selection, Propagation, H, S})
+  end;
 
+% Passive section, node has sent its message and waits for a response.
+% Returns the new view after having handled all the received messages.
+listen(NodeId, waiting, View, {Selection, Propagation, H, S}) ->
+  receive
     {push, FromPid, Cycle, ReceivedBuffer} ->
       % Passive section, updates local view with received view.
       % If strategy is pushpull, first respond to sender with local buffer.
       NewView = utils:receivedBuffer(NodeId, self(), FromPid, Cycle, View, ReceivedBuffer, {Propagation, H, S}),
-      listen(NodeId, up, NewView, {Selection, Propagation, H, S});
+      listen(NodeId, waiting, NewView, {Selection, Propagation, H, S});
 
     {response, ReceivedBuffer} ->
       % Received response to pushpull message, updates local view.
       NewView = utils:selectView(self(), View, ReceivedBuffer, H, S),
-      listen(NodeId, up, NewView, {Selection, Propagation, H, S});
-
-    {kill} ->
-      % Stop the peer-sampling service on the node
-      listen(NodeId, down, View, {Selection, Propagation, H, S})
+      listen(NodeId, waiting, NewView, {Selection, Propagation, H, S})
+  after 1500 ->
+    View
   end.
+
+
+%%% LOGS %%%
+
+% Logs the cycle, the node ID, and the node's full view.
+log(Cycle, NodeId, View) ->
+  io:format("~p;~p;~p~n", [Cycle, NodeId, viewToLog(View)]).
+
+% Converts the view into a printable list of peer IDs.
+viewToLog(View) ->
+  viewToLog(View, []).
+viewToLog([], Acc) ->
+  lists:reverse(Acc);
+viewToLog([{PeerId, _, _}|T], Acc) ->
+  viewToLog(T, [PeerId|Acc]).
