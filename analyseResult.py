0f9efbff92aaffb9f6da08ce95a10fce900a4e8f
jebogaert <jeremie.bogaert@student.uclouvain.be>
2020-12-06 16:06:47 +0100

    analyse des resultats

--- analyseResult.py
+++ analyseResult.py
@@ -1,31 +1,30 @@
 import matplotlib.pyplot as plt
+import os
 
-matPlot = []
-listDictForIndegree = []
-with open("result.txt") as f:
-    lines = f.readlines()
-    listDictForIndegree = [{} for i in range(len(lines/20))]
-    for i in lines:
-        words = i.split(",").strip()
-        for j in words[2:]:
-            listDictForIndegree[int(words[0])//20 - 1][j] = listDictForIndegree[int(words[0])//20 - 1].get(j, 0) + 1
-
-
-xss = []
-yss = []
-for i in listDictForIndegree:
-    xs = []
-    ys = []
-    for j in i:
-        xs.append(j)
-        ys.append(i[j])
-    xss.append(xs)
-    yss.append(ys)
-
-#xss matrice ou chaque ligne contient le nom des noeuds
-#yss matrice ou chaque ligne contient l'indegree des noeuds
-
-
-
-
-#format =  Cycle, NodeID, NodeID1, NodeID2, ...
+for file in os.listdir("./graphs"):
+    matPlot = []
+    listDictForIndegree = []
+    with open("./graphs/"+file, "r") as f:
+        print(file)
+        lines = f.readlines()
+        listDictForIndegree = [{} for i in range(9)]
+        for i in lines:
+            #format =  Cycle;NodeID;[NodeID1,NodeID2,....]
+            parts = i.replace("\n", "").split(";")
+            if int(parts[0])%20 == 0:
+                for j in parts[2][1:-1].split(","):
+                    listDictForIndegree[int(parts[0])//20 - 1][j] = listDictForIndegree[int(parts[0])//20 - 1].get(j, 0) + 1
+    xss = []
+    yss = []
+    #xss matrice ou chaque ligne contient le nom des noeuds
+    #yss matrice ou chaque ligne contient l'indegree des noeuds
+    for i in listDictForIndegree:
+        xs = []
+        ys = []
+        for j in i:
+            xs.append(int(j))
+            ys.append(int(i[j]))
+        xss.append(xs)
+        yss.append(ys)
+    plt.boxplot(xss)
+    plt.show()
